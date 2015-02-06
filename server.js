5330413c6d8c8d6ac6bd865dfe622b0f84c6d0c0
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-06 00:45:54 -0700

    set up Express server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3030;
+
+app.set('port', port);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
