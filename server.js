50c548a5fce6d7fb61b4bbb54a1f6d3a752beb74
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-06 09:17:47 -0700

    add un/subscribe API to server

--- server.js
+++ server.js
@@ -120,6 +120,29 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
   if (req, query.genre) {
