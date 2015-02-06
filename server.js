8692d376484a73c38697844f2a41d04e195e29bc
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-06 10:05:34 -0700

    add missing middlewares

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express = require('express');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
+var crypto = require('crypto');
 var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var compress = require('compression');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
 
 var agenda = require('agenda')({ db: { address: 'mongodb://kingsten:abcd1234@ds041651.mongolab.com:41651/tvtracker'} });
 
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
+var _ = require('lodash');
+
+var tokenSecret = 'your unique secret';
 
 var showSchema = new mongoose.Schema({
   _id: Number,
@@ -91,33 +96,44 @@ var Show = mongoose.model('Show', showSchema);
 mongoose.connect('mongodb://kingsten:abcd1234@ds041651.mongolab.com:41651/tvtracker');
 
 var app = express();
-var port = process.env.PORT || 3030;
 
-app.set('port', port);
-app.use(compress());
+app.set('port', process.env.PORT || 3030);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(session({
-  secret: 'keyboard cat'
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) next();
-  else res.send(401);
+  if (req.headers.authorization) {
+    var token = req.headers.authorization.split(' ')[1];
+    try {
+      var decoded = jwt.decode(token, tokenSecret);
+      if (decoded.exp <= Date.now()) {
+        res.send(400, 'Access token has expired');
+      } else {
+        req.user = decoded.user;
+        return next();
+      }
+    } catch (err) {
+      return res.send(500, 'Error parsing token');
+    }
+  } else {
+    return res.send(401);
+  }
 }
 
-app.post('/api/login', passport.authenticate('local'), function(req, res) {
-  res.cookie('user', JSON.stringify(req.user));
-  res.send(req.user);
-});
+function createJwtToken(user) {
+  var payload = {
+    user: user,
+    iat: new Date().getTime(),
+    exp: moment().add('days', 7).valueOf()
+  };
+  return jwt.encode(payload, tokenSecret);
+}
 
-app.post('/api/signup', function(req, res, next) {
+app.post('/auth/signup', function(req, res, next) {
   var user = new User({
+    name: req.body.name,
     email: req.body.email,
     password: req.body.password
   });
@@ -127,46 +143,106 @@ app.post('/api/signup', function(req, res, next) {
   });
 });
 
-app.get('/api/logout', function(req, res, next) {
-  req.logout();
-  res.send(200);
+app.post('/auth/login', function(req, res, next) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (!user) return res.send(401, 'User does not exist');
+    user.comparePassword(req.body.password, function(err, isMatch) {
+      if (!isMatch) return res.send(401, 'Invalid email and/or password');
+      var token = createJwtToken(user);
+      res.send({
+        token: token
+      });
+    });
+  });
 });
 
-app.use(function(req, res, next) {
-  if (req.user) {
-    res.cookie('user', JSON.stringify(req.user));
+app.post('/auth/facebook', function(req, res, next) {
+  var profile = req.body.profile;
+  var signedRequest = req.body.signedRequest;
+  var encodedSignature = signedRequest.split('.')[0];
+  var payload = signedRequest.split('.')[1];
+
+  var appSecret = '298fb6c080fda239b809ae418bf49700';
+
+  var expectedSignature = crypto.createHmac('sha256', appSecret).update(payload).digest('base64');
+  expectedSignature = expectedSignature.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+
+  if (encodedSignature !== expectedSignature) {
+    return res.send(400, 'Invalid Request Signature');
   }
-  next();
-});
 
-app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
-  Show.findById(req.body.showId, function(err, show) {
-    if (err) return next(err);
-    show.subscribers.push(req.user.id);
-    show.save(function(err) {
+  User.findOne({
+    facebook: profile.id
+  }, function(err, existingUser) {
+    if (existingUser) {
+      var token = createJwtToken(existingUser);
+      return res.send(token);
+    }
+    var user = new User({
+      name: profile.name,
+      facebook: {
+        id: profile.id,
+        email: profile.email
+      }
+    });
+    user.save(function(err) {
       if (err) return next(err);
-      res.send(200);
+      var token = createJwtToken(user);
+      res.send(token);
     });
   });
 });
 
-app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
-  Show.findById(req.body.showId, function(err, show) {
-    if (err) return next(err);
-    var index = show.subscribers.indexOf(req.user.id);
-    show.subscribers.splice(index, 1);
-    show.save(function(err) {
+app.post('/auth/google', function(req, res, next) {
+  var profile = req.body.profile;
+  User.findOne({
+    google: profile.id
+  }, function(err, existingUser) {
+    if (existingUser) {
+      var token = createJwtToken(existingUser);
+      return res.send(token);
+    }
+    var user = new User({
+      name: profile.displayName,
+      google: {
+        id: profile.id,
+        email: profile.emails[0].value
+      }
+    });
+    user.save(function(err) {
       if (err) return next(err);
-      res.send(200);
+      var token = createJwtToken(user);
+      res.send(token);
     });
   });
 });
+
+app.get('/api/users', function(req, res, next) {
+  if (!req.query.email) {
+    return res.send(400, {
+      message: 'Email parameter is required.'
+    });
+  }
+
+  User.findOne({
+    email: req.query.email
+  }, function(err, user) {
+    if (err) return next(err);
+    res.send({
+      available: !user
+    });
+  });
+});
+
+
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
-  if (req, query.genre) {
+  if (req.query.genre) {
     query.where({
-      genre: rqe.query.genre
+      genre: req.query.genre
     });
   } else if (req.query.alphabet) {
     query.where({
@@ -189,23 +265,23 @@ app.get('/api/shows/:id', function(req, res, next) {
 });
 
 app.post('/api/shows', function(req, res, next) {
+  var seriesName = req.body.showName
+    .toLowerCase()
+    .replace(/ /g, '_')
+    .replace(/[^\w-]+/g, '');
   var apiKey = '9EF1D1E7D28FDA0B';
   var parser = xml2js.Parser({
     explicitArray: false,
     normalizeTags: true
   });
-  var seriesName = req.body.showName
-    .toLowerCase()
-    .replace(/ /g, '_')
-    .replace(/[^\w-]+/g, '');
 
   async.waterfall([
     function(callback) {
       request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
         if (error) return next(error);
-        parser.parserString(body, function(error, result) {
+        parser.parseString(body, function(err, result) {
           if (!result.data.series) {
-            return res.send(404, {
+            return res.send(400, {
               message: req.body.showName + ' was not found.'
             });
           }
@@ -215,7 +291,7 @@ app.post('/api/shows', function(req, res, next) {
       });
     },
     function(seriesId, callback) {
-      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(err, response, body) {
+      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(error, response, body) {
         if (error) return next(error);
         parser.parseString(body, function(err, result) {
           var series = result.data.series;
@@ -224,6 +300,7 @@ app.post('/api/shows', function(req, res, next) {
             _id: series.id,
             name: series.seriesname,
             airsDayOfWeek: series.airs_dayofweek,
+            airsTime: series.airs_time,
             firstAired: series.firstaired,
             genre: series.genre.split('|').filter(Boolean),
             network: series.network,
@@ -244,6 +321,7 @@ app.post('/api/shows', function(req, res, next) {
               overview: episode.overview
             });
           });
+          callback(err, show);
         });
       });
     },
@@ -253,15 +331,15 @@ app.post('/api/shows', function(req, res, next) {
         url: url,
         encoding: null
       }, function(error, response, body) {
-        show.poster = 'data' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+        show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
         callback(error, show);
       });
     }
   ], function(err, show) {
     if (err) return next(err);
-    show.save(function(error) {
+    show.save(function(err) {
       if (err) {
-        if (err.code === 11000) {
+        if (err.code == 11000) {
           return res.send(409, {
             message: show.name + ' already exists.'
           });
@@ -276,6 +354,30 @@ app.post('/api/shows', function(req, res, next) {
     });
   });
 });
+
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    show.subscribers.push(req.user._id);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user._id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
 app.get('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
 });
@@ -287,8 +389,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 agenda.define('send email alert', function(job, done) {
@@ -296,7 +398,13 @@ agenda.define('send email alert', function(job, done) {
     name: job.attrs.data
   }).populate('subscribers').exec(function(err, show) {
     var emails = show.subscribers.map(function(user) {
-      return user.email;
+      if (user.facebook) {
+        return user.facebook.email;
+      } else if (user.google) {
+        return user.google.email
+      } else {
+        return user.email
+      }
     });
 
     var upcomingEpisode = show.episodes.filter(function(episode) {
@@ -327,7 +435,7 @@ agenda.define('send email alert', function(job, done) {
   });
 });
 
-agenda.start();
+//agenda.start();
 
 agenda.on('start', function(job) {
   console.log("Job %s starting", job.attrs.name);
