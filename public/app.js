875ad2ae4426fd3b09d0530b0b4f6cb41f203d7d
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-06 01:18:28 -0700

    add RESTful API for routing

--- public/app.js
+++ public/app.js
@@ -10,6 +10,33 @@ angular
   ])
   .config(_config);
 
-function _config() {
+_config.$inject = ['$locationProvider', '$routeProvider'];
 
+function _config($locationProvider, $routeProvider) {
+  $locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'MainController'
+    })
+    .when('/shows/:id', {
+      templateUrl: 'views/detail.html',
+      controller: 'DetailController'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    })
+    .when('/signup', {
+      templateUrl: 'views/signup.html',
+      controller: 'SignupController'
+    })
+    .when('/add', {
+      templateUrl: 'views/add.html',
+      controller: 'AddController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 }
