a40c447aaedd9eb54af71f50ee41e5cb16fd2548
Kingsten Banh <kingstenbanh@gmail.com>
2015-02-06 01:06:47 -0700

    add navigation bar

---
+++ public/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular
+  .module('app', [
+    'ngCookies',
+    'ngResource',
+    'ngMessages',
+    'ngRoute',
+    'mgcrea.ngStrap'
+  ])
+  .config(_config);
+
+function _config() {
+
+}
